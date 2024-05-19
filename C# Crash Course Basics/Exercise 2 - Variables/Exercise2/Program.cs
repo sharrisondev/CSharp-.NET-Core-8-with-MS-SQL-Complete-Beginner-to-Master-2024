a793cb85dbaed0f64db3831bcf345829b3865c02
sharrisondev <shaun_harrison@icloud.com>
2024-05-19 18:16:39 +0100

    Started the bootcamps basic exercises

---
+++ C# Crash Course Basics/Exercise 2 - Variables/Exercise2/Program.cs
@@ -0,0 +1,7 @@
+﻿string myString = "this is my string";
+decimal myDecimal = 20.00M;
+bool myBoolean = true;
+
+Console.WriteLine(myString.GetType());
+Console.WriteLine(myDecimal.GetType());
+Console.WriteLine(myBoolean.GetType());
---
+++ C# Crash Course Basics/Exercise 3 - Data Structures/Exercise3/Program.cs
@@ -0,0 +1,7 @@
+﻿Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>();
+itemPrices.Add("cheese", 5.99M);
+itemPrices.Add("carrots", 2.99M);
+
+Console.WriteLine(itemPrices["cheese"].GetType());
+Console.WriteLine(itemPrices["cheese"]);
+Console.WriteLine(itemPrices["carrots"]);
